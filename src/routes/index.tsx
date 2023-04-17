b1f694066b0f0470f5223b6ca49d99d60a214563
xxxanon <90407942+xxxanon@users.noreply.github.com>
2023-04-17 16:23:54 +0000

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,11 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>Thanks for your order</Title>
+      <h1>Processing!</h1>
       <Counter />
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
+       
       </p>
     </main>
   );
